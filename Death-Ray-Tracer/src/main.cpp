d6c56ac0d14a83e0c43357d9f84bfbb67678c16e
Liljan <marcus.t.lilja@gmail.com>
2017-06-08 20:26:42 +0200

    Window.

---
+++ Death-Ray-Tracer/src/main.cpp
@@ -0,0 +1,93 @@
+#include "GL/glew.h"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include "glfwContext.h"
+#include <iostream>
+#include <fstream>
+
+void input_handler(GLFWwindow* _window, double _dT);
+void GL_calls();
+
+int main() {
+	glfwContext glfw;
+	GLFWwindow* current_window = nullptr;
+
+	glfw.init(300, 200, "Procedural Planet Maker");
+	glfw.getCurrentWindow(current_window);
+	glfwSetInputMode(current_window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+	// Setup ImGui binding
+	ImGui_ImplGlfw_Init(current_window, true);
+
+	//start GLEW extension handler
+	glewExperimental = GL_TRUE;
+	GLenum l_GlewResult = glewInit();
+	if (l_GlewResult != GLEW_OK)
+		std::cout << "glewInit() error." << std::endl;
+
+	float value = 0.0f;
+
+	// RENDER LOOP \__________________________________________/
+
+	while (!glfwWindowShouldClose(current_window))
+	{
+		glfwPollEvents();
+
+		ImGui_ImplGlfw_NewFrame();
+		{
+			ImGui::Begin("Death Ray Tracer");
+
+			ImGui::SliderFloat("Elevation", &value, 0.0f, 0.2f);
+			/*if (ImGui::IsItemHovered())
+				ImGui::SetTooltip("A tooltip"); */
+
+			ImGui::End();
+		}
+
+		//glfw input handler
+		//input_handler(current_window, delta_time);
+
+		// __________ RENDERING SETTINGS _______
+
+		GL_calls();
+
+		glUseProgram(0);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+		// Rendering imgui
+		int display_w, display_h;
+		glfwGetFramebufferSize(current_window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		ImGui::Render();
+
+		glfwSwapBuffers(current_window);
+	}
+
+	ImGui_ImplGlfw_Shutdown();
+	return 0;
+}
+
+
+void input_handler(GLFWwindow* _window, double _dT)
+{
+	if (glfwGetKey(_window, GLFW_KEY_ESCAPE)) {
+		glfwSetWindowShouldClose(_window, GL_TRUE);
+	}
+}
+
+
+void GL_calls()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	//glEnable(GL_CULL_FACE);
+	//glCullFace(GL_BACK);
+	glDisable(GL_TEXTURE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glEnable(GL_ALPHA_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
