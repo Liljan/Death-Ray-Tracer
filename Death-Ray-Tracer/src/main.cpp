c9a85d8de6adbcb4346da3f092801417d22aefc4
Liljan <marcus.t.lilja@gmail.com>
2017-06-13 13:58:25 +0200

    GUI for options.

--- Death-Ray-Tracer/src/main.cpp
+++ Death-Ray-Tracer/src/main.cpp
@@ -5,15 +5,41 @@
 #include <iostream>
 #include <fstream>
 
+#include <iostream>
+#include <string>
+
 #include "Image.h"
 
+int width = 1080, height = 720;
+int rays_per_pixel = 1;
+float fov = 45.0f;
+
+static char file_name_buffer[64] = "file_name";
+static const char* FILE_ENDING = ".ppm";
+
+static char* render_message = "Ready to render...";
+
 void GL_calls();
+
+void render() {
+	Image img(width, height);
+
+	render_message = "Rendering...";
+	img.fillImage(glm::vec3(1.0f, 1.0f, 0.4f));
+
+	std::string full_path = file_name_buffer;
+	full_path.append(FILE_ENDING);
+
+	img.saveAsPPM(full_path.c_str());
+
+	render_message = "Successfully rendered image";
+}
 
 int main() {
 	glfwContext glfw;
 	GLFWwindow* current_window = nullptr;
 
-	glfw.init(300, 200, "Procedural Planet Maker");
+	glfw.init(400, 200, "Procedural Planet Maker");
 	glfw.getCurrentWindow(current_window);
 	glfwSetInputMode(current_window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
 
@@ -38,7 +64,21 @@ int main() {
 		{
 			ImGui::Begin("Death Ray Tracer");
 
-			ImGui::SliderFloat("Elevation", &value, 0.0f, 0.2f);
+			ImGui::InputInt("Width", &width);
+			ImGui::InputInt("Height", &height);
+
+			ImGui::InputInt("Rays per pixel", &rays_per_pixel, 1, 1);
+			ImGui::InputFloat("Field of view", &fov, 1.0f, 1.5f, 1.0f);
+
+			ImGui::InputText("File name", file_name_buffer, sizeof(file_name_buffer));
+
+			if (ImGui::Button("Render"))
+				render();
+
+			ImGui::Separator();
+			ImGui::Spacing();
+
+			ImGui::Text(render_message);
 
 			ImGui::End();
 		}
@@ -60,10 +100,6 @@ int main() {
 	}
 
 	ImGui_ImplGlfw_Shutdown();
-
-	Image img(100,100);
-	img.fillImage(glm::vec3(1.0f, 1.0f, 0.4f));
-	img.saveAsPPM("Brett.ppm");
 
 	return 0;
 }
