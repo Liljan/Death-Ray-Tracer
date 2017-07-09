3213600ea702371eefca08a968f45c8968eba46d
Liljan <marcus.t.lilja@gmail.com>
2017-07-09 17:38:36 +0200

    Camera clean-up.

--- Death Ray Tracer/include/Camera.h
+++ Death Ray Tracer/include/Camera.h
@@ -4,19 +4,13 @@
 
 class Camera {
 public:
-	Camera(glm::mat4 camera_to_world);
 	Camera(glm::vec3 position, glm::mat4 rotation);
 	Camera() {};
-	
-	glm::mat4 get_camera_to_world() { return camera_to_world; }
 
 	glm::vec3 get_position() { return _position; }
 	glm::mat4 get_rotation() { return _rotation; }
 
 private:
-	glm::mat4 camera_to_world;
-	glm::mat4 world_to_camera;
-
 	glm::mat4 _rotation;
 	glm::vec3 _position;
 };
